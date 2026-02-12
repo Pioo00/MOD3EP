792e851378481241f5b23dff61171619c11bb15b
malmonte-zip <mary.antoinette.almonte@student.ateneo.edu>
2026-02-12 19:34:07 +0800

    Made the Supplier Model [8a]

--- M3E-Models-AlmonteGarciaJose/MyInventorySystem/MyInventoryApp/models.py
+++ M3E-Models-AlmonteGarciaJose/MyInventorySystem/MyInventoryApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def getName(self):
+        return self.name
+    
+    def __str__(self):
+        return "{} - {}, {} created at: {}".format(self.name, self.city, self.country, self.created_at)
