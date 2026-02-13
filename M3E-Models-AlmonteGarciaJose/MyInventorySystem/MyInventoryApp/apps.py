e150a0c2ad9359d922b1bc599887bb6a925cea07
malmonte-zip <mary.antoinette.almonte@student.ateneo.edu>
2026-02-13 11:53:55 +0800

    apps and urls

--- M3E-Models-AlmonteGarciaJose/MyInventorySystem/MyInventoryApp/apps.py
+++ M3E-Models-AlmonteGarciaJose/MyInventorySystem/MyInventoryApp/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from django.contrib import admin
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
 
 class MyinventoryappConfig(AppConfig):
     name = 'MyInventoryApp'
